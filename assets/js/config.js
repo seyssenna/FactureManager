170ca541857d6a1364189bcdcdaa920c7f235e6d
millet <millet.mathieu.0@gmail.com>
2022-04-10 18:40:13 +0200

    Retrieve API_URL from .env with webpack(dotenv)

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL = process.env.API_URL;
 export const INVOICES_API_URL = API_URL + "invoices";
 export const CUSTOMERS_API_URL = API_URL + "customers";
 export const USERS_API_URL = API_URL + "users";
